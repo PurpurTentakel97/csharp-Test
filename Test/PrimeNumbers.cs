fe23b928b7430b159df56f4a37fd796715bf8646
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-05-10 21:34:04 +0200

    Prime Numbers

---
+++ Test/PrimeNumbers.cs
@@ -0,0 +1,43 @@
+﻿namespace Numbers
+{
+    internal class PrimeNumbers
+    {
+        public static void PrintPrimeNumbersTillX(int maxNumber)
+        {
+            if (maxNumber <= 0)
+            {
+                Console.WriteLine("Zahl kleiner Null");
+                return;
+            }
+
+            if (maxNumber > 10000)
+            {
+                Console.WriteLine("Zahl zu groß");
+                return;
+            }
+
+            for (int i = 2; i < maxNumber; i++)
+            {
+                bool found = false;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (i % j == 0)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    Console.Write($"{i}, ");
+                }
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Test/Programm.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Purpur Tentakel
+ * 02.05.2022
+ * Test - Main
+ * C# v. 10.0
+ */
+
+using Numbers;
+
+namespace Test
+{
+    class Programm
+    {
+        static string countName = "Baum";
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Geben sie eine Zahl ein. ('quit' um das Programm zu beenden.)");
+                string input = Console.ReadLine();
+                if (input == "quit")
+                {
+                    break;
+                }
+                if (input is null)
+                {
+                    Console.WriteLine("keine Zahl eingegeben");
+                    continue;
+                }
+                int input_int = 0;
+                try
+                {
+                    input_int = int.Parse(input);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("keine Ganzzahlzahl eingegeben");
+                    continue;
+                }
+                PrimeNumbers.PrintPrimeNumbersTillX(maxNumber: input_int);
+
+            }
+            Console.WriteLine("Programm beendet");
+
+        }
+
+        private static void PrintBreak()
+        {
+            Console.WriteLine("|----------------------------------------------------------------------------------------------------|");
+        }
+
+        private static void PrintCount()
+        {
+            Console.WriteLine(countName + " " + count.ToString());
+            Console.WriteLine($"{countName} {count.ToString()}");
+            count++;
+        }
+
+    }
+}
