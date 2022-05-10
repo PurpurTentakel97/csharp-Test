01dce4e1ebb5b07fd3f3c26fe3504bcf58eb83cd
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-05-10 20:28:54 +0200

    test

--- Test/main.cs
+++ Test/main.cs
@@ -5,17 +5,28 @@
  * C# v. 10.0
  */
 
+using Lists;
+
 
 namespace Test
 {
     class Program
     {
-        static int listCount = 0;
+        static string countName = "Baum";
+        static int count = 0;
         static void Main(string[] args)
         {
+            var entry = new string[] { "a", "b", "c", "d", "d" };
 
+            PrintCount();
+            var list_1 = new LinkedList();
+            list_1.Print();
+            PrintBreak();
 
-
+            PrintCount();
+            var list_2 = new LinkedList(entry);
+            list_2.Print();
+            PrintBreak();
         }
 
         private static void PrintBreak()
@@ -23,10 +34,10 @@ namespace Test
             Console.WriteLine("|----------------------------------------------------------------------------------------------------|");
         }
 
-        private static void PrintListCount()
+        private static void PrintCount()
         {
-            Console.WriteLine("Liste " + listCount.ToString());
-            listCount++;
+            Console.WriteLine(countName + " " + count.ToString());
+            count++;
         }
 
     }
